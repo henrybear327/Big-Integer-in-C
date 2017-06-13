293103f1f6f105bad72cae16a8a739b1706225ce
Henry Tseng <henrybear327@gmail.com>
2017-06-13 21:09:55 +0800

    Use struct

--- bigInt.c
+++ bigInt.c
@@ -5,59 +5,59 @@
 #define N 40
 
 typedef struct {
-    int num[N];
+    int num[2 * N];
     int isNegative;
 } Number;
 
-void add(int num1[], int num2[], int ans[])
+void add(Number *num1, Number *num2, Number *ans)
 {
     for (int i = 0; i < N; i++) {
-        int sum = num1[i] + num2[i];
-        ans[i + 1] += sum / 10;
-        ans[i] += sum % 10;
+        int sum = num1->num[i] + num2->num[i];
+        ans->num[i + 1] += sum / 10;
+        ans->num[i] += sum % 10;
     }
 
     for (int i = 0; i < N; i++) {
-        ans[i + 1] += ans[i] / 10;
-        ans[i] %= 10;
+        ans->num[i + 1] += ans->num[i] / 10;
+        ans->num[i] %= 10;
     }
 }
 
-void subtract(int num1[], int num2[], int ans[])
+void subtract(Number *num1, Number *num2, Number *ans)
 {
     int borrow[N + 1] = {0};
     for (int i = 0; i < N; i++) {
-        int diff = borrow[i] + (num1[i] - num2[i]);
+        int diff = borrow[i] + (num1->num[i] - num2->num[i]);
 
         if (diff < 0) {
             borrow[i + 1] = -1;
             diff += 10;
         }
 
-        ans[i] = diff;
+        ans->num[i] = diff;
     }
 }
 
-void multiply(int num1[], int num2[], int ans[])
+void multiply(Number *num1, Number *num2, Number *ans)
 {
     for (int i = 0; i < N; i++) {   /* num2 */
         for (int j = 0; j < N; j++) { /* num1 */
-            int product = num1[j] * num2[i];
-            ans[i + j] += product;
+            int product = num1->num[j] * num2->num[i];
+            ans->num[i + j] += product;
         }
     }
 
     for (int i = 0; i < 2 * N - 1; i++) {
-        ans[i + 1] += ans[i] / 10;
-        ans[i] %= 10;
+        ans->num[i + 1] += ans->num[i] / 10;
+        ans->num[i] %= 10;
     }
 }
 
-void printNumber(int ans[])
+void printNumber(Number ans)
 {
     int notZero = 0;
     for (int i = 2 * N - 1; i >= 0; i--) {
-        if (ans[i] == 0)
+        if (ans.num[i] == 0)
             continue;
         else {
             notZero = i;
@@ -65,39 +65,53 @@ void printNumber(int ans[])
         }
     }
 
+    if (ans.isNegative == 1)
+        printf("-");
     for (int i = notZero; i >= 0; i--)
-        printf("%d", ans[i]);
+        printf("%d", ans.num[i]);
     printf("\n");
 }
 
-void convertFromStringToNumberArray(char str[], int num[])
+void convertFromStringToNumber(char str[], Number *num)
 {
     int len = strlen(str);
-    for (int i = 0; i < len; i++)
-        num[i] = str[len - i - 1] - '0';
+    if (str[0] == '-')
+        num->isNegative = 1;
+    else
+        num->isNegative = 0;
+
+    // -1234
+    for (int i = 0; i < len - num->isNegative; i++)
+        num->num[i] = str[len - i - 1] - '0';
 }
 
 int main()
 {
     char number1[2 * N], number2[2 * N];
     while (scanf("%s %s", number1, number2) != EOF) {
-        int num1[2 * N] = {0}, num2[2 * N] = {0};
-        convertFromStringToNumberArray(number1, num1);
-        convertFromStringToNumberArray(number2, num2);
+        Number num1, num2, ans;
+        convertFromStringToNumber(number1, &num1);
+        convertFromStringToNumber(number2, &num2);
+
+        printNumber(num1);
+        printNumber(num2);
 
         printf("\nAddition\n");
-        int ans[2 * N] = {0}; /* no more overflow issue */
-        add(num1, num2, ans);
+        ans.isNegative = 0;
+        memset(ans.num, 0, sizeof(ans.num));
+        add(&num1, &num2, &ans);
         printNumber(ans);
 
         printf("\nSubtraction\n");
-        memset(ans, 0, sizeof(ans));
-        subtract(num1, num2, ans);
+        ans.isNegative = 0;
+        memset(ans.num, 0, sizeof(ans.num));
+        subtract(&num1, &num2, &ans);
         printNumber(ans);
 
         printf("\nMultiplication\n");
-        memset(ans, 0, sizeof(ans));
-        multiply(num1, num2, ans);
+        ans.isNegative = 0;
+        memset(ans.num, 0, sizeof(ans.num));
+        multiply(&num1, &num2, &ans);
         printNumber(ans);
     }
 
